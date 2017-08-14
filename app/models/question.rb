bb2153cafb780083f95d5e5513f3cbde78333f77
Aleksandr Klimenkov <prisioner@mail.ru>
2017-08-14 17:08:08 +0300

    update user and question validation

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,11 @@ class Question < ApplicationRecord
   belongs_to :user
 
   validates :text, :user, presence: true
+  validates :text, length: { maximum: 255 }
+
+  before_save :prepare_text
+
+  def prepare_text
+    self.text.strip!
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,38 @@ class User < ApplicationRecord
   validates :username, :email, presence: true
   validates :username, :email, uniqueness: true
 
+  # my 'compromise' between /.+@.+/ and monster-style validation :)
+  # one or more any symbols before '@', except '@'
+  # only one '@'
+  # one or more (sub)domain groups (one or more non-'@' symbols with point at end)
+  # ends with domain-zone - one or more symbols except point or '@'
+  validates :email, format: { with: /\A[^@]+@([^@\.]+\.)+[^@\.]+\Z/ }
+
+  # at least 2 characters (maximum = 40)
+  # starts with latin letter
+  # may include non-following underscopes in the middle
+  # may include numbers
+  validates :username, length: { in: 2..40 }
+  validates :username, format: { with: /\A[a-z](_?[a-z\d]+)+\Z/, message: 'only latin symbols and underscopes' }
+
+
   attr_accessor :password
 
   validates_presence_of :password, on: :create
   validates_confirmation_of :password
 
   before_save :encrypt_password
+  before_validation :prepare_attributes
+
+  def prepare_attributes
+    self.username.strip!
+    self.username.downcase!
+
+    self.email.strip!
+    self.email.downcase!
+
+    self.name.strip! if self.name.present?
+  end
 
   def encrypt_password
     if self.password.present?
@@ -31,6 +57,7 @@ class User < ApplicationRecord
   end
 
   def self.authenticate(email:, password:)
+    email.downcase!
     user = find_by(email: email)
 
     if user.present? && user.password_hash == User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST))
