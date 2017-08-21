355aba1088b10fa79b41d0da271d9ba01dacf7f4
Aleksandr Klimenkov <prisioner@mail.ru>
2017-08-21 16:01:59 +0300

    add colors reset button

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -21,3 +21,17 @@ $(document).on('input', '#user_username', function(){
 $(document).on('input', '#user_name', function(){
   $('#user_name_sandbox').text($(this).val());
 });
+
+$(function(){
+  $('#user_reset_colors').click(function(){
+    $('#user_avatar_bg_color').val('#005a55');
+    $('.user-header').css('background', '#005a55');
+
+    $('#user_avatar_border_color').val('#00b6ad');
+    $('.user-image-div').css('border-color', '#00b6ad');
+    
+    $('#user_profile_text_color').val('#ffffff');
+    $('.user-header').css('color', '#ffffff');
+    return false;
+  });
+});
