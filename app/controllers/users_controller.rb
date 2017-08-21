8f5fad5e9976b295d6697863ae9c1d9c35fefc17
Aleksandr Klimenkov <prisioner@mail.ru>
2017-08-21 06:16:03 +0300

    user logged in after registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_url, notice: I18n.t('pages.users.new.user.created')
     else
       render 'new'
