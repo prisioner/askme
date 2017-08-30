3660e8301d6584d4969e8c13fef2930081acb43e
Aleksandr Klimenkov <prisioner@mail.ru>
2017-08-30 22:19:02 +0300

    linked tags questions added to view

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -9,5 +9,17 @@ class TagsController < ApplicationController
     @tag = Tag.find_by(alias: tag_alias.mb_chars.downcase.to_s)
 
     @questions = @tag.questions.order(created_at: :desc)
+
+    # собираем список вопросов от похожих хэштегов
+    linked_tags_all_questions =
+        @tag.
+            linked_tags.
+            flat_map(&:questions).
+            uniq.
+            sort_by { |q| q.created_at }.
+            reverse
+
+    # Убираем повторы
+    @linked_tags_questions = linked_tags_all_questions - @questions.to_a
   end
 end
