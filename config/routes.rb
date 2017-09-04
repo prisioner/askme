a488cf49e9e9bece302ae67ca6db7b19ab1c2190
Aleksandr Klimenkov <prisioner@mail.ru>
2017-09-04 13:59:02 +0300

    security fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,7 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users
-  resources :sessions, only: [:new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
   resources :questions, except: [:show, :new, :index]
   resources :tags, only: :show, param: :alias
-
-  get 'sign_up' => 'users#new'
-  get 'log_in' => 'sessions#new'
-  get 'log_out' => 'sessions#destroy'
 end
